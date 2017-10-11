603b5ba6eb1571c30343b9037c09229afd9251aa
Brian Holland <neumaics@gmail.com>
2017-10-10 22:25:41 -0600

    cleaning up generated files

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,19 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const clean = require('gulp-clean');
 
-gulp.task('sass', function () {
+gulp.task('sass', ['clean'], () => {
   return gulp.src('./src/**/*.scss')
     .pipe(sass().on('error', sass.logError))
+    .pipe(clean())
     .pipe(gulp.dest('./dist'));
 });
 
-gulp.task('sass:watch', function () {
+gulp.task('sass:watch', () => {
   gulp.watch('./src/**/*.scss', ['sass']);
+});
+
+gulp.task('clean', () => {
+  return gulp.src('./dist', {read: false})
+    .pipe(clean());
 });
